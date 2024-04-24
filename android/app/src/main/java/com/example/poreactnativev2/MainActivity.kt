05d8b96abf09dccce1eca2c1e3477919ee50725c
Alex <cebotari.alex@gmail.com>
2024-04-24 21:41:45 +0100

    Working example

--- android/app/src/main/java/com/example/poreactnativev2/MainActivity.kt
+++ android/app/src/main/java/com/example/poreactnativev2/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.poreactnativev2
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.poreactnativev2.ui.theme.POReactNativeV2Theme
+import com.facebook.react.ReactInstanceManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +27,46 @@ class MainActivity : ComponentActivity() {
             POReactNativeV2Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
+                    val context = LocalContext.current
+                    Column {
+
+                        Button(onClick = {
+
+                            if (!Settings.canDrawOverlays(context)) {
+                                val intent = Intent(
+                                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                    Uri.parse("package: $packageName")
+                                )
+                                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+                            }
+                        }) {
+                            Text(text = "Check permissions")
+                        }
+
+                        Button(onClick = {
+                            context.startActivity(Intent(context, MyReactActivity::class.java))
+                        }) {
+                            Text(text = "Start view")
+                        }
+                    }
                 }
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                // SYSTEM_ALERT_WINDOW permission not granted
+            }
+        }
+        ReactInstanceManager.builder().build()?.onActivityResult(this, requestCode, resultCode, data)
+    }
+
+    companion object {
+        const val OVERLAY_PERMISSION_REQ_CODE = 1  // Choose any value
     }
 }
 
